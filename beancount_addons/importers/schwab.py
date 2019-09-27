f52d5a7a4b08e841efe43e60884bba1375022d68
David Ciani <dciani@davidciani.com>
2019-09-26 21:37:12 -0700

    Initial commit

---
+++ beancount_addons/importers/schwab.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from beancount.core.number import D
+from beancount.ingest import importer
+from beancount.core import account
+from beancount.core import amount
+from beancount.core import flags
+from beancount.core import data
+from beancount.core.position import Cost
+
+from dateutil.parser import parse
+
+from titlecase import titlecase
+
+import csv
+import os
+import re
+
+class SchwabBankImporter(importer.ImporterProtocol):
+    def __init__(self, account, lastfour):
+        self.account = account
+        self.lastfour = lastfour
+
+    def identify(self, f):
+        return re.match('XXXXXX.*{}_Checking_Transactions_.*\.CSV'.format(self.lastfour), os.path.basename(f.name))
+
+    def extract(self, f):
+        entries = []
+
+        with open(f.name) as f:
+            for _ in range(3): # first 3 lines are garbage
+                next(f)
+
+            for index, row in enumerate(csv.reader(f)):
+                trans_date = parse(row[0]).date()
+                trans_desc = titlecase(row[3])
+
+                if row[4]:
+                    trans_amt = float(row[4].strip('$')) * -1.
+                elif row[5]:
+                    trans_amt = float(row[5].strip('$'))
+                else:
+                    continue # 0 dollar transaction
+
+                trans_amt = '{:.2f}'.format(trans_amt)
+
+                meta = data.new_metadata(f.name, index)
+
+                txn = data.Transaction(
+                    meta=meta,
+                    date=trans_date,
+                    flag=flags.FLAG_OKAY,
+                    payee=trans_desc,
+                    narration="",
+                    tags=set(),
+                    links=set(),
+                    postings=[],
+                )
+
+                txn.postings.append(
+                    data.Posting(self.account, amount.Amount(D(trans_amt),
+                        'USD'), None, None, None, None)
+                )
+
+                entries.append(txn)
+
+        return entries
